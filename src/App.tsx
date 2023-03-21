341273f864e4fed9d8174e8c8cb289ca9aed0975
Alexis 'seith' M <amontelimart@gmail.com>
2023-03-21 05:37:34 +0100

    :tada: hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+import { useScript } from "usehooks-ts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+	const status = useScript(`/Gradient.js`, {
+		removeOnUnmount: false,
+	})
+
+
+	useEffect(() => {
+		if (status === "ready") {
+			const canvasElement = document.getElementById("gradient-canvas");
+			const gradient: any = new Gradient();
+			if (canvasElement) {
+				gradient.initGradient("#gradient-canvas");
+			} else {
+				gradient.pause();
+			}
+
+		}
+	}, [status]);
+
+
+	return (
+		<>
+			<canvas id="gradient-canvas" data-transition-in />
+			<div id="app" className="App">
+				<div className="pageContainer">
+					<div className="userApp card">
+						<img src="https://github.com/menonparik/xmb-on-web/blob/main/images/video.png?raw=true" />
+					</div>
+					<div className="userApp card">
+						<img src="https://github.com/menonparik/xmb-on-web/blob/main/images/video.png?raw=true" />
+					</div>
+					<div className="userApp card">
+						<img src="https://github.com/menonparik/xmb-on-web/blob/main/images/video.png?raw=true" />
+					</div>
+					<div className="userApp card">
+						<img src="https://github.com/menonparik/xmb-on-web/blob/main/images/video.png?raw=true" />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
-
-export default App;
